52f0aee86fdde586c1fa26118cf077d8f4442a13
Luisa Vaca <gguevara@hugeinc.com>
2016-09-20 16:17:43 -0500

    portfolio: setup and first modules setup

---
+++ application/App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import Fullviewport from './common/js/full-square.js';
+
+ var App = React.createClass({
+  render() {
+    return <div> Hello world </div>
+  }
+})
+
+export default App
---
+++ application/common/js/full-square.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import data from '../../data/data.json';
+
+var fullView = React.createClass({
+  render() {
+    <section></section>
+  }
+})
